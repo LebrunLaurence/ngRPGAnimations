9cf71f99dd57db48d81443600b4a39d8775b482f
Laurence Lebrun <1936047@cegepmontpetit.ca>
2025-11-07 12:08:33 -0500

    10.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {transition, trigger, useAnimation} from "@angular/animations";
-import { jello, pulse, shakeX } from 'ng-animate';
+import { bounce, flip, jello, pulse, shakeX } from 'ng-animate';
+import { lastValueFrom, timer } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -28,6 +29,24 @@ import { jello, pulse, shakeX } from 'ng-animate';
       useAnimation(jello, { params: { timing: 0.5 } })
     ),
   ]),
+  trigger("bounce", [
+    transition(
+      ":increment",
+      useAnimation(bounce, { params: { timing: 1 } })
+    ),
+  ]),
+  trigger("shake", [
+    transition(
+      ":increment",
+      useAnimation(shakeX, { params: { timing: 0.75 } })
+    ),
+  ]),
+  trigger("flip", [
+    transition(
+      ":increment",
+      useAnimation(flip, { params: { timing: 0.75 } })
+    ),
+  ]),
 ]
 })
 export class AppComponent {
@@ -35,8 +54,16 @@ export class AppComponent {
   ng_death = 0;
   ng_attack = 0;
   ng_pattack = 0;
+  ng_bounce = 0;
+  ng_shake = 0;
+  ng_flip = 0;
 
   css_hit = false;
+
+  css_spin = false;
+  css_flip = false;
+
+  keepSpinning = false;
 
   constructor() {
   }
@@ -70,6 +97,38 @@ export class AppComponent {
     setTimeout(() => {
       this.css_hit = false;
     }, 1000);
+  }
+
+  async threethings(){
+    this.ng_bounce++;
+
+    await lastValueFrom(timer(1000));
+
+    this.ng_shake++;
+
+    await lastValueFrom(timer(750));
+
+    this.ng_flip++;
+  }
+
+  spin(){
+    this.css_spin = true;
+    this.keepSpinning = true;
+
+    setTimeout(() => {
+      this.css_spin = false;
+      this.css_flip = true;
+    }, 1600);
+
+    setTimeout(() => {
+      this.css_spin = false;
+    }, 2300);
+
+    setTimeout(() => {
+      if(this.keepSpinning){
+        this.spin();
+      }
+    }, 3000);
   }
 
   showSlime(){
